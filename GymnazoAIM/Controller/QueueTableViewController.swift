afce05ffc44931e123af228d719e9a54f466c232
Austin McInnis <austin.mcinnis@icloud.com>
2018-06-11 03:38:41 -0700

    Workout Videos successfully being created and updated!!!

--- GymnazoAIM/Controller/QueueTableViewController.swift
+++ GymnazoAIM/Controller/QueueTableViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Austin McInnis. All rights reserved.
 //
 
+import AVFoundation
 import Firebase
 import UIKit
 
@@ -13,8 +14,22 @@ class QueueTableViewController: UITableViewController, QueueTableDelegate, UITex
 
     private var doneAction: UIAlertAction?
     var queue:[Exercise]?
+    var videoURLs = [URL]() {
+        didSet {
+            if let queue = self.queue {
+                if videoURLs.count == queue.count {
+                    self.downloadAlert.dismiss(animated: true, completion: {
+                        self.present(self.rendderAlert, animated: true, completion: nil)
+                    })
+                    buildWorkoutVideo()
+                }
+            }
+        }
+    }
     var workout:Workout?
     
+    private var downloadAlert = UIAlertController(title: "Downloading...", message: "Gathering exercise videos...", preferredStyle: .alert)
+    private var rendderAlert = UIAlertController(title: "Rendering...", message: "Creating workout video...", preferredStyle: .alert)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,6 +66,140 @@ class QueueTableViewController: UITableViewController, QueueTableDelegate, UITex
         tableView.reloadData()
     }
     
+    func downloadVideos() {
+        guard self.workout != nil else {
+            print("Null workout!")
+            return
+        }
+        
+        if let exercises = self.queue {
+            // Build Workout Video
+            
+            self.present(self.downloadAlert, animated: true, completion: nil)
+            
+            //Get each video from Firebase Storage
+            let storageRef = Storage.storage().reference()
+            for exercise in exercises {
+                if let storagePath = exercise.storagePath {
+                    let videoRef = storageRef.child(storagePath)
+                    videoRef.downloadURL(completion: {
+                        [weak self] (url, error) in
+                        guard error == nil else {
+                            if let error = error {
+                                print(error.localizedDescription)
+                            }
+                            return
+                        }
+                        guard let this = self else { return }
+                        if let url = url {
+                            this.videoURLs.append(url)
+                        }
+                    })
+                }
+            }
+        }
+    }
+    
+    func buildWorkoutVideo() {
+        var assets = [AVAsset]()
+        for url in videoURLs {
+            assets.append(AVAsset(url: url))
+        }
+        
+        KVVideoManager.shared.merge(arrayVideos: assets, completion: {
+            [weak self] (outputURL, error) in
+            guard let this = self else { return }
+            
+            if let error = error {
+                print("Error:\(error.localizedDescription)")
+            }
+            else {
+                if let url = outputURL {
+                    print("Output video file:\(url)")
+                    if let workout = this.workout {
+                        let video = Video(url: url)
+                        workout.video = video
+                        this.rendderAlert.dismiss(animated: true, completion: nil)
+                        this.pushWorkout()
+                    }
+                }
+            }
+        })
+    }
+    
+    func pushWorkout() {
+        if let workout = self.workout, let name = workout.name {
+            //Firebase
+            let workoutsRef = Database.database().reference().child("workouts")
+            let workoutRef = workoutsRef.childByAutoId()
+            let videoRef = Storage.storage().reference().child("workouts").child(workoutRef.key)
+            
+            if let workout = self.workout {
+                //Database
+                workout.databaseID = workoutRef.key
+                workout.databasePath = "workouts/" + workout.databaseID!
+                
+                workoutRef.setValue([
+                    "name": name,
+                    "databaseID": workoutRef.key,
+                    "databasePath": workout.databasePath!,
+                    "exercises": workout.getExercisePaths(),
+                    "storagePath": workout.databasePath!
+                    ])
+                
+                //Storage
+                if let video = workout.video {
+                    video.name = workout.name
+                    
+                    let progressAlert = UIAlertController(title: "Progress", message: "", preferredStyle: .alert)
+                    let progressBar = UIProgressView(progressViewStyle: .bar)
+                    self.present(progressAlert, animated: true, completion: nil)
+                    
+                    let uploadTask = videoRef.putFile(from: video.url, metadata: nil) {
+                        [weak self] (metadata, error) in
+                        guard let this = self else { return }
+                        if let error = error {
+                            print("Error uploading to Storage!: \(error.localizedDescription)")
+                            return
+                        }
+                        
+                        let successAlert = UIAlertController(title: "Success", message: "'\(video.name!)' uploaded successfully.", preferredStyle: .alert)
+                        successAlert.addAction(UIAlertAction(title: "Done", style: .default, handler: {
+                            [weak self] (action) in
+                            guard let this = self else { return }
+                            this.dismiss(animated: true, completion: nil)
+                        }))
+                        this.present(successAlert, animated: true, completion: nil)
+                        print("Upload Success!")
+                    }
+                    
+                    uploadTask.observe(.resume, handler: {
+                        (snapshot) in
+                        progressAlert.message = "0% Uploaded"
+                        let margin = 8.0
+                        let frame = CGRect(x: margin, y: 72.0, width: Double(progressAlert.view.frame.width) - margin * 2.0, height: 2.0)
+                        progressBar.frame = frame
+                        progressAlert.view.addSubview(progressBar)
+                    })
+                    
+                    uploadTask.observe(.progress, handler: {
+                        (snapshot) in
+                        let progress = Double(snapshot.progress!.completedUnitCount) / Double(snapshot.progress!.totalUnitCount)
+                        progressBar.setProgress(Float(progress), animated: true)
+                        progressAlert.message = "\(String(format: "%3.1f", 100.0*progress))% Uploaded"
+                    })
+                    
+                    uploadTask.observe(.success, handler: {
+                        [weak self] (snapshot) in
+                        guard let this = self else { return }
+                        this.dismiss(animated: true, completion: nil)
+                    })
+//                    self.present(progressAlert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
     @IBAction func upload(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "New Workout", message: "Please enter the title of the workout", preferredStyle: .alert)
         alert.addTextField(configurationHandler: {
@@ -69,32 +218,18 @@ class QueueTableViewController: UITableViewController, QueueTableDelegate, UITex
             [weak self] (action) in
             guard let this = self else { return }
             
+            
             let textField = alert.textFields?.first
             var workoutName:String?
             if let textField = textField {
                 workoutName = textField.text!
                 print(textField.text!)
             }
-            
-            if let name = workoutName, let exercises = this.queue {
-                this.workout = Workout(name: name, exercises: exercises)
-                
-                //Firebase Database
-                let workoutsRef = Database.database().reference().child("workouts")
-                let workoutRef = workoutsRef.childByAutoId()
-                
-                if let workout = this.workout {
-                    workout.databaseID = workoutRef.key
-                    workout.databasePath = "workouts/" + workout.databaseID!
-                    
-                    workoutRef.setValue([
-                        "name": name,
-                        "databaseID": workoutRef.key,
-                        "databasePath": workout.databasePath!,
-                        "exercises": workout.getExercisePaths()
-                    ])
-                }
+            if this.workout == nil, let name = workoutName, let queue = this.queue {
+                this.workout = Workout(name: name, exercises: queue)
             }
+            this.getQueue()
+            this.downloadVideos()
         })
         doneAction?.isEnabled = false
         alert.addAction(doneAction!)
