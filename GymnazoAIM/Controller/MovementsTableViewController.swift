3a00ad178a0464cd7a8f41283424dbc30fa048ec
Austin McInnis <austin.mcinnis@icloud.com>
2018-06-11 17:47:06 -0700

    Removed NewExerciseViewController and added Action Sheet.

--- GymnazoAIM/Controller/MovementsTableViewController.swift
+++ GymnazoAIM/Controller/MovementsTableViewController.swift
@@ -6,14 +6,18 @@
 //  Copyright © 2017 Austin McInnis. All rights reserved.
 //
 
+import AVKit
 import Firebase
 import UIKit
 
-class MovementsTableViewController: UITableViewController {
+class MovementsTableViewController: UITableViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
-    var movements = [GlobalMovement]()
     var exercises = [Exercise]()
+    private var imagePicker = UIImagePickerController()
+    var movements = [GlobalMovement]()
+    @IBOutlet var newExerciseButton: UIBarButtonItem!
     private var selectedMovement: GlobalMovement?
+    private var video: Video?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +27,11 @@ class MovementsTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.mediaTypes = ["public.movie"]
+        
         let globalMovementsRef = Database.database().reference().child("category_names").child("Global Movements")
         
         globalMovementsRef.observe(.childAdded, with: {
@@ -62,6 +71,100 @@ class MovementsTableViewController: UITableViewController {
         return cell
     }
     
+    // MARK: - Create New Exercise
+    @IBAction func newExercise(_ sender: Any) {
+        let optionMenu = UIAlertController(title: nil, message: "Choose Option", preferredStyle: .actionSheet)
+        let recordAction = UIAlertAction(title: "Video", style: .default) {
+            [weak self] (action) in
+            guard let this = self else { return }
+            this.recordExercise()
+        }
+        let importAction = UIAlertAction(title: "Library", style: .default) {
+            [weak self] (action) in
+            guard let this = self else { return }
+            this.importExercise()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) {
+            [weak self] (action) in
+            guard let this = self else { return }
+            this.dismiss(animated: true, completion: nil)
+        }
+        
+        optionMenu.popoverPresentationController?.barButtonItem = self.newExerciseButton
+        optionMenu.addAction(recordAction);
+        optionMenu.addAction(importAction);
+        optionMenu.addAction(cancelAction); // Removed by UIKit. Tap anywhere outside to cancel
+        self.present(optionMenu, animated: true, completion: nil)
+    }
+    
+    func recordExercise() {
+        let cameraMediaType = AVMediaType.video
+        let cameraAuthorizationStatus = AVCaptureDevice.authorizationStatus(for: cameraMediaType)
+        
+        switch cameraAuthorizationStatus {
+        case .denied: break
+        case .authorized: presentCamera()
+        case .restricted: break
+            
+        case .notDetermined:
+            // Prompting user for the permission to use the camera.
+            AVCaptureDevice.requestAccess(for: cameraMediaType) {
+                [weak self] granted in
+                guard let this = self else { return }
+                
+                if granted {
+                    print("Granted access to \(cameraMediaType)")
+                    this.presentCamera()
+                } else {
+                    print("Denied access to \(cameraMediaType)")
+                }
+            }
+        }
+    }
+    
+    func presentCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+            present(imagePicker, animated: true, completion: nil)
+        }
+        else {
+            print ("Camera not available")
+        }
+    }
+    
+    func importExercise() {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    //MARK: - Image Picker Controller Delegate
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let videoURL = info[UIImagePickerControllerMediaURL] as? URL
+        
+        if let url = videoURL {
+            video = Video(url: url)
+            //            player = AVPlayer(url: url)
+            //            avController.player = player
+            
+            do {
+                let attr = try FileManager.default.attributesOfItem(atPath: url.path)
+                let creationDate = attr[FileAttributeKey.creationDate] as? Date
+                if let date = creationDate {
+                    video?.creationDate = date
+                }
+            } catch  {
+                
+            }
+        }
+        
+        imagePicker.dismiss(animated: true, completion: {
+            [weak self] in
+            guard let this = self else { return }
+            this.performSegue(withIdentifier: "CategorizeNewExercise", sender: nil)
+        })
+    }
+    
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         let movement = movements[indexPath.row]
         
@@ -76,42 +179,7 @@ class MovementsTableViewController: UITableViewController {
         
         return indexPath
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingdidStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -122,6 +190,16 @@ class MovementsTableViewController: UITableViewController {
         if segue.identifier == "movementSelected" {
             if let exVC = segue.destination as? ExercisesTableViewController {
                 exVC.movement = selectedMovement
+            }
+        }
+        
+        if segue.identifier == "CategorizeNewExercise" {
+            if let nav = segue.destination as? UINavigationController {
+                if let dest = nav.topViewController as? CategorizeNewExerciseViewController {
+                    if let video = video {
+                        dest.video = video
+                    }
+                }
             }
         }
     }
